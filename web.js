dcecce1649d8f1b8e41e7a9f817f032cea156d12
Matthew Iglesias <matthewiglesias@gmail.com>
2013-07-10 06:23:21 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,18 +2,18 @@ var express = require('express'), fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-//var content = fs.readFileSync("Hello", 'utf8');
+var content = fs.readFileSync('/etc/passwd', 'utf8');
 
 
 app.get('/', function(request, response) {
   //response.send('Hello World2!');
-   fs.readFileSync('index.html',function (err){
-      if (err) throw err;
+   //fs.readFileSync('index.html',function (err){
+   //   if (err) throw err;
       buf = new Buffer(256);
       len = buf.write(content,0);
      response.send(buf.toString('utf8',0,len);
      // console.log(buf.toString('utf8',0,len);
-   }
+  // }
 });
 
 var port = process.env.PORT || 5000;
